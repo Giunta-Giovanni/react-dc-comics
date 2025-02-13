6d8b1a1cc0c3c5bcb6f8526519743e1563df97e6
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-13 17:08:59 +0100

    [feat] footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,90 @@ export default function Footer() {
         <footer>
             <div className="footer-link">
                 <div className="container">
+                    {/* box link */}
+                    <div className="box-link d-flex">
+                        <div className="link-col">
+                            <h5>DC COMICS</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">Characters</a>
+                                </li>
+                                <li>
+                                    <a href="#">Comics</a>
+                                </li>
+                                <li>
+                                    <a href="#">Movies</a>
+                                </li>
+                                <li>
+                                    <a href="#">TV</a>
+                                </li>
+                                <li>
+                                    <a href="#">Games</a>
+                                </li>
+                                <li>
+                                    <a href="#">Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                </li>
+                            </ul>
+
+                            <h5>SHOP</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">Shop DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">Shop DC Collectibles</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="link-col">
+                            <h5>DC</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">Terms Of Use</a>
+                                </li>
+                                <li>
+                                    <a href="#">Privacy Policy</a>
+                                </li>
+                                <li>
+                                    <a href="#">Advertising</a>
+                                </li>
+                                <li>
+                                    <a href="#">Jobs</a>
+                                </li>
+                                <li>
+                                    <a href="#">Subscriptions</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="link-col">
+                            <h5>SITES</h5>
+                            <ul>
+                                <li>
+                                    <a href="#">DC</a>
+                                </li>
+                                <li>
+                                    <a href="#">MAD Magazine</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Kids</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Universe</a>
+                                </li>
+                                <li>
+                                    <a href="#">DC Power Visa</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
             </div>
             <div className="footer-social">
-                <div className="container social">
+                <div className="container social ">
                     <div className="row">
                         {/* colonna sinistra */}
                         <div className="col">
@@ -24,26 +103,31 @@ export default function Footer() {
                                     <a href="#">
                                         <img src="./img/footer-facebook.png" alt="" />
                                     </a>
+
                                 </li>
                                 <li className="single-channel">
                                     <a href="">
                                         <img src="./img/footer-twitter.png" alt="" />
                                     </a>
+
                                 </li>
                                 <li className="single-channel">
                                     <a href="">
                                         <img src="./img/footer-youtube.png" alt="" />
                                     </a>
+
                                 </li>
                                 <li className="single-channel">
                                     <a href="">
                                         <img src="./img/footer-pinterest.png" alt="" />
                                     </a>
+
                                 </li>
                                 <li className="single-channel">
                                     <a href="">
                                         <img src="./img/footer-periscope.png" alt="" />
                                     </a>
+
                                 </li>
                             </ul>
                         </div>
@@ -52,6 +136,6 @@ export default function Footer() {
 
 
             </div>
-        </footer>
+        </footer >
     )
 }
