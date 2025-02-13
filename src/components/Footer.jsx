b22692fc2d7b3b22ac4052c1ab577b5a019bd18a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-13 08:34:08 +0100

    [feat] footer draft

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
 export default function Footer() {
     return (
         <footer>
-            /* questo è il footer*/
-        </footer>
+            {/* riga */}
+            <div className="row">
+                {/* colonna sinistra */}
+                <div className="col">
+                    {/* pulsante iscriviti */}
+                    <button type="button" className="btn-sign">SIGN-UP NOW!</button>
+                </div>
+                {/* colonna destra */}
+                <div className="col col-chl">
+                    {/* canali da seguire */}
+                    <p>FOLLOW US</p>
+                    <ul className="channel">
+                        <li className="single-channel">
+                            <a href="#">
+                                <img src="./img/footer-facebook.png" alt="" />
+                            </a>
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <img src="./img/footer-twitter.png" alt="" />
+                            </a>
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <img src="./img/footer-youtube.png" alt="" />
+                            </a>
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <img src="./img/footer-pinterest.png" alt="" />
+                            </a>
+                        </li>
+                        <li className="single-channel">
+                            <a href="">
+                                <img src="./img/footer-periscope.png" alt="" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </footer >
     )
 }
