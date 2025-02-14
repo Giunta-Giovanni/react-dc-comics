7d97a43296d465c1a1c96f80360d70724e00be77
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-14 16:39:18 +0100

    [feat] import blue bar & jumbo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,55 +1,20 @@
+import BlueBar from "./BlueBar";
+import Jumbo from "./Jumbo";
+
 export default function Main() {
     return (
         <main>
+            <Jumbo />
+
             <section className="content-box">
                 <div className="container">
-                    <h2> Content goes here </h2>
+
+
                 </div>
             </section>
-            <section className="blue-bar">
-                <div className="container d-flex justify-content-around text-white py-3">
-                    <div className="card-link">
-                        <a href="#">
-                            <div className="box-card-img">
-                                <img src="./img/buy-comics-digital-comics.png" alt="Digital Comics" />
-                            </div>
-                            <p>DIGITAL COMICS</p>
-                        </a>
-                    </div>
-                    <div className="card-link">
-                        <a href="#">
-                            <div className="box-card-img">
-                                <img src="./img/buy-comics-merchandise.png" alt="DC Merchandise" />
-                            </div>
-                            <p>DC MERCHANDISE</p>
-                        </a>
-                    </div>
-                    <div className="card-link">
-                        <a href="#">
-                            <div className="box-card-img">
-                                <img src="./img/buy-comics-subscriptions.png" alt="Subscription" />
-                            </div>
-                            <p>SUBSCRIPTION</p>
-                        </a>
-                    </div>
-                    <div className="card-link">
-                        <a href="#">
-                            <div className="box-card-img">
-                                <img src="./img/buy-comics-shop-locator.png" alt="Comic Shop Locator" />
-                            </div>
-                            <p>COMIC SHOP LOCATOR</p>
-                        </a>
-                    </div>
-                    <div className="card-link">
-                        <a href="#">
-                            <div className="box-card-img">
-                                <img src="./img/buy-dc-power-visa.svg" alt="DC Power Visa" />
-                            </div>
-                            <p>DC POWER VISA</p>
-                        </a>
-                    </div>
-                </div>
-            </section>
+
+            <BlueBar />
+
         </main >
 
     );
