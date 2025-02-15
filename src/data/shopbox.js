f5c7cfdab4c2938283d8523a42fe1e9e4b8806f4
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-15 14:12:44 +0100

    [feat] add array with element of blue bar

---
+++ src/data/shopbox.js
@@ -0,0 +1,24 @@
+const shopBox = [
+    {
+        imgSrc: './img/buy-comics-digital-comics.png',
+        label: 'DIGITAL COMICS'
+    },
+    {
+        imgSrc: './img/buy-comics-merchandise.png',
+        label: 'DC MERCHANDISE'
+    },
+    {
+        imgSrc: './img/buy-comics-subscriptions.png',
+        label: 'SUBSCRIPTION'
+    },
+    {
+        imgSrc: './img/buy-comics-shop-locator.png',
+        label: 'COMIC SHOP LOCATOR'
+    },
+    {
+        imgSrc: './img/buy-dc-power-visa.svg',
+        label: 'DC POWER VISA'
+    }
+];
+
+export default shopBox
